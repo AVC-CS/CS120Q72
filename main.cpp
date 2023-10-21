7a459545f1405f7cd825ef830b3060b6bcfe01b7
Kayw Lee <kwooler@gmail.com>
2023-10-20 23:21:06 -0700

    Q72

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+	/*****************************************
+	1	2	3
+	4	5	6
+	7 	8	9
+	*****************************************/
 	int numbers[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int rows = 3;
 	int cols = 3;
@@ -16,6 +21,26 @@ int main()
 	cout << endl;
 	col = 2;
 	getCol(numbers, rows, cols, col, result);
+	for (int i = 0; i < rows; i++)
+		cout << result[i] << "\t";
+	cout << endl;
+
+	int numbers2[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+	/*****************************************
+	1	2	3 	4	5
+	6 	7	8	9	10
+	11	12 	13	14	15
+	16	17 	18	19	20
+	*****************************************/
+	rows = 4;
+	cols = 5;
+	row = 4;
+	getRow(numbers2, rows, cols, row, result);
+	for (int i = 0; i < cols; i++)
+		cout << result[i] << "\t";
+	cout << endl;
+	col = 5;
+	getCol(numbers2, rows, cols, col, result);
 	for (int i = 0; i < rows; i++)
 		cout << result[i] << "\t";
 	cout << endl;
--- main.hpp
+++ main.hpp
@@ -11,13 +11,13 @@ void getRow(int numbers[], int rows, int cols, int row, int result[])
 {
     for (int i = 0; i < cols; i++)
     {
-        result[i] = numbers[(row - 1) * rows + i];
+        result[i] = numbers[(row - 1) * cols + i];
     }
 }
 void getCol(int numbers[], int rows, int cols, int col, int result[])
 {
     for (int i = 0; i < rows; i++)
     {
-        result[i] = numbers[(col - 1) + (rows * i)];
+        result[i] = numbers[(col - 1) + (cols * i)];
     }
 }
