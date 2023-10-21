e3f3b25e634d8a775f87946737635939b5ed53bf
Kyuwoong Lee <kwooler@gmail.com>
2023-10-20 18:33:59 -0700

    Initial commit

---
+++ main.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+const int SIZE = 3;
+void printtriangle(int numbers[][SIZE]);
+
+void printtriangle(int num[][SIZE])
+{
+    ofstream ofs;
+
+    ofs.close();
+}
